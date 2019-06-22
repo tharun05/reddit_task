ff4b34876eff7c65b6f8210c5252795ba77fe9fa
Tharun <Tharunesh.Dillikar1@T-Mobile.com>
2019-06-23 04:14:40 +0530

    component test case

--- src/app/components/subredditsearch/subredditsearch.component.spec.ts
+++ src/app/components/subredditsearch/subredditsearch.component.spec.ts
@@ -6,10 +6,12 @@ import {TranslateModule, TranslateService} from '@ngx-translate/core';
 import {FormsModule} from '@angular/forms';
 import {RedditService} from '../../shared/services/reddit.service';
 import { HttpClientModule } from '@angular/common/http';
+import {By} from '@angular/platform-browser';
 
 describe('SubredditsearchComponent', () => {
   let component: SubredditsearchComponent;
   let fixture: ComponentFixture<SubredditsearchComponent>;
+
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -33,5 +35,19 @@ describe('SubredditsearchComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('Set Button Text to Something', () => {
+    const defaultButtonText = 'subReddit.search';
+    fixture.detectChanges();
+    const button = fixture.debugElement.query(By.css('.btn-secondary'));
+    expect(button.nativeElement.textContent.trim()).toBe(defaultButtonText);
+  });
+
+  it('Set anchor tag Text to ', () => {
+    const defaultButtonText = 'RedditLite';
+    fixture.detectChanges();
+    const button = fixture.debugElement.query(By.css('.navbar-brand'));
+    expect(button.nativeElement.textContent.trim()).toBe(defaultButtonText);
   });
 });
